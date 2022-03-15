6b36ec619eaf751748537cbab0a0381f40fe9ff5
Cole Gibbs <colegibbs0@gmail.com>
2022-03-15 15:02:52 -0700

    refractored using .map

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 class HornedBeast extends React.Component {
   render() {
-    const photo = this.props.url;
     return(
       <>
-          <h3>{this.props.title}</h3>
-          <img src={photo} alt={this.props.alt} title={this.props.title}/>
-          <p>{this.props.description}</p>
+        <h3>{this.props.title}</h3>
+        <img src={this.props.imageURL} alt={this.props.alt} title={this.props.title}/>
+        <p>{this.props.description}</p>
       </>
     );
   }
