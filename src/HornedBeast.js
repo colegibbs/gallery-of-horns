7e33e9cfac99ba799dda9b29e064a4ee30f55d83
Cole Gibbs <colegibbs0@gmail.com>
2022-03-15 15:22:25 -0700

    import HornedBeast.css

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import './HornedBeast.css'
 
 class HornedBeast extends React.Component {
   render() {
     return(
-      <>
+      <article>
         <h3>{this.props.title}</h3>
         <img src={this.props.imageURL} alt={this.props.alt} title={this.props.title}/>
         <p>{this.props.description}</p>
-      </>
+      </article>
     );
   }
 }
