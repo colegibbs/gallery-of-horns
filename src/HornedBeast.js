de1df55d89d1677f1f3e0a9483d83c11c0876be2
Cole Gibbs <colegibbs0@gmail.com>
2022-03-14 17:30:17 -0700

    created HornedBeast component

---
+++ src/HornedBeast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+  render() {
+    return(
+        <>
+          <h2>Beast Title</h2>
+          <img src="img-url" alt="beast"/>
+          <p>Description</p>
+        </>
+    );
+  }
+}
+
+export default HornedBeast;
