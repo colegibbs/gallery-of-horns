b4c2472dc562b3caeee9e7cfeed400e0948c08bb
Cole Gibbs <colegibbs0@gmail.com>
2022-03-15 15:49:19 -0700

    import and use bootstrap button

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import './HornedBeast.css'
 
 class HornedBeast extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      votes: 0,
+    }
+  }
+  handleVotes = () => {
+    this.setState({
+      votes: this.state.votes + 1,
+    });
+  }
   render() {
     return(
       <article>
         <h3>{this.props.title}</h3>
+        <p>{this.state.votes ? `${this.state.votes}❤️` : ''}</p>
         <img src={this.props.imageURL} alt={this.props.alt} title={this.props.title}/>
         <p>{this.props.description}</p>
+        <Button onClick={this.handleVotes}>Vote For {this.props.title}</Button>
       </article>
     );
   }
