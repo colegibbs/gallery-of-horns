e1e816f0c98544722f5873fed6724b06c5fb3693
Cole Gibbs <colegibbs0@gmail.com>
2022-03-14 21:13:33 -0700

    images render correctly

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import photo from './profile-photo-100-100 copy.jpeg';
 
 class HornedBeast extends React.Component {
   render() {
     return(
-        <>
-          <h2>Beast Title</h2>
-          <img src="img-url" alt="beast"/>
-          <p>Description</p>
-        </>
+      <>
+          <h3>{this.props.title}</h3>
+          <img src={photo} alt="beast"/>
+          <p>{this.props.description}</p>
+      </>
     );
   }
 }
