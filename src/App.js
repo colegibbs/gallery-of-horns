bfb39cc66ebec0c314dff6a836f0d67e949e3f87
Cole Gibbs <colegibbs0@gmail.com>
2022-03-16 15:44:42 -0700

    ids added on modal img for styling

--- src/App.js
+++ src/App.js
@@ -34,12 +34,19 @@ class App extends React.Component{
           showModal={this.showModal}
         />
         <Footer/>
-        <Modal show ={this.state.showModal} onHide={this.hideModal}>
+        <Modal 
+          show ={this.state.showModal} 
+          onHide={this.hideModal}
+        >
           <Modal.Header closeButton>
           <Modal.Title>{this.state.title}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <img src ={this.state.urlImage} alt ='beast'></img>
+            <img 
+              src ={this.state.urlImage} 
+              alt ={this.state.title}
+              id="modal-img">
+            </img>
           </Modal.Body>
         </Modal>
       </>
