7e83404d047f8537109ae00013a3ba94ea28b5b6
Cole Gibbs <colegibbs0@gmail.com>
2022-03-17 15:55:19 -0700

    refractor data filter

--- src/Main.js
+++ src/Main.js
@@ -4,8 +4,17 @@ import './Main.css';
 
 class Main extends React.Component {
   render() {
-    console.log(this.props);
-    let beasts = this.props.data.map((beast, index) => {
+    console.log(this.props.hornNum);
+    console.log(this.props.data[0].horns);
+    let selectedBeast;
+    if(this.props.hornNum === "all"){
+      selectedBeast = this.props.data;
+    }
+    else {
+      selectedBeast = this.props.data.filter((beast) => beast.horns === +this.props.hornNum);
+    }
+    console.log(selectedBeast);
+    let beasts = selectedBeast.map((beast, index) => {
       return(
         <HornedBeast
           title={beast.title}
